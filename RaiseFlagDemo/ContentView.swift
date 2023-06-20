75388f7f09e451e30183f242c87619dc7d278872
Mousa Sweidan <msweidan@hct.ac.ae>
2023-06-20 16:42:05 +0400

    Initial Commit

---
+++ RaiseFlagDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RaiseFlagDemo
+//
+//  Created by msweidan on 20/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RaiseFlagDemo/RaiseFlagDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaiseFlagDemoApp.swift
+//  RaiseFlagDemo
+//
+//  Created by msweidan on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct RaiseFlagDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
